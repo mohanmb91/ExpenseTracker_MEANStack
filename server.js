1ab3a09543b0a9f32806439829c5e17742fb777b
mohanmb91 <mohankumarmbuniv@gmail.com>
2017-06-14 18:00:50 -0700

    mongo Express angular Node JS set up done

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
-var express = require("express");
+var express = require("express"),
+    stylus = require("stylus"),
+    logger = require("morgan"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV  || 'development';
 
-
 var app = express();
+
+function compile(str,path){
+    return stylus(str).set('filename',path);
+}
 
 app.set('views',__dirname+ '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+mongoose.connect('mongodb://localhost/expensemanager');
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'conntection error ... '));
+db.once('open',function callback(){
+    console.log("expense manager DB open");
+});
+
+var messageSchema = mongoose.Schema({message: "String"});
+var Message = mongoose.model('Message',messageSchema);
+var mongoMessage;
+
+Message.findOne().exec(function (err,messagedoc){
+    mongoMessage = messagedoc.message;
+});
+
+
+app.get('/partials/:partialPath',function(req,res){
+    res.render('partials/'+ req.params.partialPath);
+});
 
 app.get('*',function(req,res){
-    res.render('index');
+    res.render('index',{
+        mongoMessage: mongoMessage
+    });
 });
 
 var port = 3030;
